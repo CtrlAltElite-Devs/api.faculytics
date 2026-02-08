69b0905a7b557cf5b7f33f05499268803b959829
y4nder <lorenzolubguban@gmail.com>
2026-02-08 13:31:12 +0800

    refactor: used class validator and transformers for moodle responses

--- src/modules/moodle/lib/moodle.types.ts
+++ src/modules/moodle/lib/moodle.types.ts
@@ -1,59 +1,9 @@
-export type MoodleTokenResponse = {
-  token: string;
-  privatetoken: string;
-};
-
-export type MoodleSiteInfoResponse = {
-  userid: number;
-  username: string;
-  firstname: string;
-  lastname: string;
-  fullname: string;
-  lang: string;
-  userpictureurl?: string;
-
-  userissiteadmin?: boolean;
-  usercanchangeconfig?: boolean;
-  usercanviewconfig?: boolean;
-
-  functions?: Array<{
-    name: string;
-    version: string;
-  }>;
-
-  siteurl?: string;
-  sitename?: string;
-  theme?: string;
-};
-
-export interface MoodleCourse {
-  id: number;
-  shortname: string;
-  fullname: string;
-  displayname: string;
-
-  enrolledusercount: number;
-
-  category: number;
-
-  startdate: number; // unix timestamp
-  enddate: number; // unix timestamp
-
-  visible: 0 | 1;
-  hidden: boolean;
-
-  courseimage?: string;
-
-  overviewfiles?: MoodleCourseFile[];
-
-  timemodified: number;
-}
-
-export interface MoodleCourseFile {
-  filename: string;
-  filepath: string;
-  filesize: number;
-  fileurl: string;
-  timemodified: number;
-  mimetype: string;
-}
+export { MoodleTokenResponse } from '../dto/responses/token.response.dto';
+export {
+  MoodleSiteInfoResponse,
+  MoodleSiteFunction,
+} from '../dto/responses/site-info.response.dto';
+export {
+  MoodleCourse,
+  MoodleCourseFile,
+} from '../dto/responses/course.response.dto';

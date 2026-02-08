25cb106af7ffc2848a38b25db80b24ebb5999e44
y4nder <lorenzolubguban@gmail.com>
2026-02-08 13:48:17 +0800

    feat(moodle): added new moodle endpoints

--- src/modules/moodle/dto/requests/login-moodle.request.dto.ts
+++ src/modules/moodle/dto/requests/login-moodle.request.dto.ts
@@ -1,6 +1,6 @@
 import { IsString } from 'class-validator';
 
-export class LoginMoodleDto {
+export class LoginMoodleRequest {
   @IsString()
   username: string;
 
--- src/modules/moodle/moodle.service.ts
+++ src/modules/moodle/moodle.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { MoodleClient } from './lib/moodle.client';
 import { env } from 'src/configurations/env';
-import { LoginMoodleDto } from './dto/login-moodle.dto';
+import { LoginMoodleRequest } from './dto/requests/login-moodle.request.dto';
+import { GetSiteInfoRequest } from './dto/requests/get-site-info.request.dto';
+import { GetEnrolledCoursesRequest } from './dto/requests/get-enrolled-courses.request.dto';
 
 @Injectable()
 export class MoodleService {
@@ -9,8 +11,20 @@ export class MoodleService {
     return new MoodleClient(env.MOODLE_BASE_URL);
   }
 
-  async Login(dto: LoginMoodleDto) {
+  async Login(dto: LoginMoodleRequest) {
     const client = this.BuildMoodleClient();
     return await client.login(dto.username, dto.password);
+  }
+
+  async GetSiteInfo(dto: GetSiteInfoRequest) {
+    const client = this.BuildMoodleClient();
+    client.setToken(dto.token);
+    return await client.getSiteInfo();
+  }
+
+  async GetEnrolledCourses(dto: GetEnrolledCoursesRequest) {
+    const client = this.BuildMoodleClient();
+    client.setToken(dto.token);
+    return await client.getEnrolledCourses(dto.userId);
   }
 }

d1e5638263a90186ed4c0d9a65c03d312a29e573
Leander Lubguban <113151776+y4nder@users.noreply.github.com>
2026-02-08 11:31:28 +0800

    [Staging] boiler plate configurations (#1) (#2)
    * feat: added configurations
    
    * feat: added health endpoints
    
    * fix: fixed imports
    
    * feat: added cors logic
    
    * fix: fixed cors type definitions

---
+++ src/configurations/env/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import z from "zod";
+import { moodleEnvSchema } from "./moodle.env";
+import { serverEnvSchema } from "./server.env";
+import { corsEnvSchema } from "./cors.env";
+
+export const envSchema = z.object({
+  ...serverEnvSchema.shape,
+  ...corsEnvSchema.shape,
+  ...moodleEnvSchema.shape
+})
+
+export type Env = z.infer<typeof envSchema>;
+
+export const env = envSchema.parse(process.env);
+
+export const envPortResolve = () => env.PORT ?? 5200;

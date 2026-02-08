312deab4ecf31fb316cf88eecbab2da6561dfd94
Leander Lubguban <113151776+y4nder@users.noreply.github.com>
2026-02-08 12:37:59 +0800

    chore: added husky (#4)
    * feat: added husky
    
    * chore: added pr ci checks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
-import { Module } from "@nestjs/common";
-import HealthModule from "./modules/health/health.module";
+import { Module } from '@nestjs/common';
+import HealthModule from './modules/health/health.module';
 
 @Module({
-  imports: [HealthModule]
+  imports: [HealthModule],
 })
-
-export default class AppModule { }
+export default class AppModule {}

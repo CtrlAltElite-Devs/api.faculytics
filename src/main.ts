444e77e20de2498c559a15bd2d8e3d1e0d2233f9
y4nder <lorenzolubguban@gmail.com>
2026-02-22 09:40:40 +0800

    chore: removed default console logger on main runtime

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,13 @@ import {
   ApplyConfigurations,
   envPortResolve,
   InitializeDatabase,
-  // useNestFactoryCustomOptions,
   usePostBootstrap,
 } from './configurations/index.config';
 import AppModule from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    // useNestFactoryCustomOptions(),
-  );
-  app.useLogger(app.get(Logger));
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('trust proxy', 1);
   ApplyConfigurations(app);
   await InitializeDatabase(app);

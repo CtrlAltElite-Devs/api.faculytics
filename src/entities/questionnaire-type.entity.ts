c4bf3e4d81135ca9125306d514819bea70481873
Leander Lubguban <113151776+y4nder@users.noreply.github.com>
2026-04-12 23:21:01 +0800

    FAC-122.1 chore: add @Index expression metadata for soft-delete partial indexes (#308)

--- src/entities/questionnaire-type.entity.ts
+++ src/entities/questionnaire-type.entity.ts
@@ -2,6 +2,11 @@ import { Entity, Property, Index } from '@mikro-orm/core';
 import { CustomBaseEntity } from './base.entity';
 import { QuestionnaireTypeRepository } from '../repositories/questionnaire-type.repository';
 
+@Index({
+  name: 'questionnaire_type_code_unique',
+  expression:
+    'create unique index "questionnaire_type_code_unique" on "questionnaire_type" ("code") where "deleted_at" is null',
+})
 @Entity({ repository: () => QuestionnaireTypeRepository })
 export class QuestionnaireType extends CustomBaseEntity {
   @Property()
